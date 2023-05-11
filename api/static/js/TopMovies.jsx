01be370a49a973e02af69e17d5b6f3489a8b7ba7
Mithin Thomas <mithingthomas@gmail.com>
2023-05-11 14:29:16 -0400

    Display movie cast to frontend

--- api/static/js/TopMovies.jsx
+++ api/static/js/TopMovies.jsx
@@ -1,32 +1,44 @@
-function TopMovies() {
+const TopMovies = () => {
   const [movies, setMovies] = React.useState([]);
+  const [currMovie, setCurrMovie] = React.useState(null);
+  const [showDetails, setShowDetails] = React.useState(false);
 
   React.useEffect(() => {
-    (async function fetchData() {
+    const fetchData = async () => {
       const response = await fetch("/top");
-      const parsed = await response.json();
-      setMovies(parsed);
-    })();
-  });
+      const movieList = await response.json();
+      setMovies(movieList);
+    };
+    fetchData();
+  }, []);
+
+  const setMovieHandler = (movie_id) => {
+    setShowDetails(true);
+    setCurrMovie(movie_id);
+  };
+
+  console.log(currMovie);
 
   return (
     <React.Fragment>
-      {movies.map((movie, index) => {
-        const releaseDate = new Date(movie.release_date);
-        return (
-          <div key={index}>
-            <img
-              src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`}
-              width={250}
-            />
-            <a href={`/movies/${movie.id}`}>
-              <p>
-                {movie.title} <span>({releaseDate.getFullYear()})</span>
-              </p>
-            </a>
-          </div>
-        );
-      })}
+      {showDetails && <MovieDetails movie_id={currMovie} />}
+      {!showDetails &&
+        movies.map((movie, index) => {
+          const releaseDate = new Date(movie.release_date);
+          return (
+            <div key={index}>
+              <img
+                src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`}
+                width={250}
+              />
+              <a onClick={setMovieHandler.bind(this, movie.movie_id)}>
+                <p>
+                  {movie.title} <span>({releaseDate.getFullYear()})</span>
+                </p>
+              </a>
+            </div>
+          );
+        })}
     </React.Fragment>
   );
-}
+};

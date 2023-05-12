00e5225d41518fe545602c944ee63cad2e11be08
Mithin Thomas <mithingthomas@gmail.com>
2023-05-12 15:38:55 -0400

    Display horizontal bar graph

--- api/static/js/MovieDetails.jsx
+++ api/static/js/MovieDetails.jsx
@@ -39,50 +39,62 @@ const MovieDetails = (props) => {
     const height = 250;
 
     const x = d3
-      .scaleLinear()
-      .domain([0, d3.max(data, (d) => d.age)])
-      .range([0, width]);
+      .scaleBand()
+      .domain(data.map((d) => d.name[0]))
+      .rangeRound([margin.left, width - margin.right])
+      .padding(0.1);
 
     const y = d3
-      .scaleBand()
-      .domain(data.map((d) => d.name))
-      .rangeRound([0, 20 * data.length])
-      .padding(0.1);
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => d.age)])
+      .range([height - margin.bottom, margin.top]);
+
+    const yTitle = (g) =>
+      g
+        .append("text")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .attr("y", 10)
+        .text("Age");
+
+    const xAxis = (g) =>
+      g
+        .attr("transform", `translate(0,${height - margin.bottom})`)
+        .call(d3.axisBottom(x).tickSizeOuter(0));
+
+    const yAxis = (g) =>
+      g
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y))
+        .call((g) => g.select(".domain").remove());
 
     const svg = d3
       .select(chartRef.current)
-      .attr("width", width)
-      .attr("height", y.range()[1])
-      .attr("font-family", "sans-serif")
-      .attr("font-size", "10")
-      .attr("text-anchor", "end");
+      .attr("viewBox", [0, 0, width, height]);
 
-    const bar = svg
-      .selectAll("g")
-      .data(data)
-      .join("g")
-      .attr("transform", (d, i) => `translate(0,${y(d.name)})`);
-
-    bar
-      .append("rect")
+    svg
+      .append("g")
       .attr("fill", "steelblue")
-      .attr("width", (d) => x(d.age))
-      .attr("height", y.bandwidth() - 1);
+      .selectAll("rect")
+      .data(data)
+      .join("rect")
+      .attr("x", (d) => x(d.name[0]))
+      .attr("y", (d) => y(d.age))
+      .attr("height", (d) => y(0) - y(d.age))
+      .attr("width", x.bandwidth());
 
-    bar
-      .append("text")
-      .attr("fill", "white")
-      .attr("x", (d) => x(d.age) - 3)
-      .attr("y", (y.bandwidth() - 1) / 2)
-      .attr("dy", "0.35em")
-      .text((d) => d.age);
+    svg.append("g").call(xAxis);
+
+    svg.append("g").call(yAxis);
+
+    svg.call(yTitle);
 
     console.log("rendering");
   }, [data]);
 
   return (
     <React.Fragment>
-      <div>
+      <div style={{ width: "350px" }}>
         <svg ref={chartRef}></svg>
       </div>
       {movieDetails &&

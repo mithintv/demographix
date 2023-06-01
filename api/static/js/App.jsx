339adaea3b2390010999950186f88fd1b81cc877
Mithin Thomas <mithingthomas@gmail.com>
2023-06-01 15:56:56 -0400

    Simplify landing page

--- api/static/js/App.jsx
+++ api/static/js/App.jsx
@@ -44,112 +44,34 @@ const darkTheme = createTheme({
       main: "#2e7d32",
     },
   },
+  typography: {
+    h1: {
+      fontWeight: 700,
+    },
+    h2: {
+      fontWeight: 500,
+    },
+  },
 });
 
 const App = () => {
-  const [searchMovies, setSearchMovies] = React.useState(false);
-  const [searchResults, setSearchResults] = React.useState([]);
-  const searchRef = React.useRef(null);
-  const [searchInput, setSearchInput] = React.useState("");
-
   const nomMovieHandler = () => {
     setNomMovies(true);
   };
 
-  const searchInputHandler = async (e) => {
-    setSearchInput(searchRef.current.value);
-  };
-
-  React.useEffect(() => {
-    let timeout;
-    if (searchInput.length > 0) {
-      setSearchMovies(true);
-      timeout = setTimeout(async () => {
-        try {
-          const options = {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              search: searchInput,
-            }),
-          };
-          const response = await fetch("/", options);
-          const json = await response.json();
-          setSearchResults(json);
-        } catch (err) {
-          console.log(err);
-        }
-      }, 200);
-    } else {
-      setSearchMovies(false);
-    }
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [searchInput]);
-
-  const searchHandler = async (e) => {
-    e.preventDefault();
-    setSearchMovies(true);
-    const keyword = searchRef.current.value;
-    console.log(keyword);
-
-    try {
-      const options = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          search: keyword,
-        }),
-      };
-      const response = await fetch("/search", options);
-      const json = await response.json();
-      setSearchResults(json);
-
-      console.log(searchResults);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   return (
     <BrowserRouter>
+      <Route path="/movies/:id" component={MovieDetails} exact></Route>
       <Container
         maxWidth="lg"
         sx={{
           display: "flex",
+          height: "100vh",
           flexDirection: "column",
           justifyContent: "center",
+          alignContent: "center",
         }}
       >
-        <Paper
-          onSubmit={searchHandler}
-          component="form"
-          sx={{
-            m: "1rem 1rem",
-            p: "0.25rem 1rem",
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-        >
-          <InputBase
-            inputRef={searchRef}
-            value={searchInput}
-            id="outlined-basic"
-            name="search"
-            label=""
-            placeholder="Search Movies"
-            variant="outlined"
-            onChange={searchInputHandler}
-          />
-          <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
-            <span className="material-symbols-outlined">search</span>
-          </IconButton>
-        </Paper>
         <Link sx={{ textDecoration: "none" }} component={RouterLink} to="/">
           <Typography align="center" variant="h1">
             Demographix
@@ -158,10 +80,9 @@ const App = () => {
         <Typography color="textSecondary" variant="subtitle2" align="center">
           Visualize the diverse tapestry of on-screen talent in blockbusterfilms
         </Typography>
+        <SearchBar />
 
-        <Route path="/movies/:id" component={MovieDetails} exact></Route>
-        <BasicTabs year={new Date().getFullYear()} />
-        {searchMovies && <SearchResults results={searchResults} />}
+        {/* <BasicTabs year={new Date().getFullYear()} /> */}
       </Container>
     </BrowserRouter>
   );
---
+++ api/static/js/interface/SearchBar.jsx
@@ -0,0 +1,97 @@
+const SearchBar = (props) => {
+  const [searchMovies, setSearchMovies] = React.useState(false);
+  const [searchResults, setSearchResults] = React.useState([]);
+  const searchRef = React.useRef(null);
+  const [searchInput, setSearchInput] = React.useState("");
+
+  const searchInputHandler = async (e) => {
+    setSearchInput(searchRef.current.value);
+  };
+
+  const searchHandler = async (e) => {
+    e.preventDefault();
+    setSearchMovies(true);
+    const keyword = searchRef.current.value;
+    console.log(keyword);
+
+    try {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          search: keyword,
+        }),
+      };
+      const response = await fetch("/search", options);
+      const json = await response.json();
+      setSearchResults(json);
+
+      console.log(searchResults);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  React.useEffect(() => {
+    let timeout;
+    if (searchInput.length > 0) {
+      setSearchMovies(true);
+      timeout = setTimeout(async () => {
+        try {
+          const options = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              search: searchInput,
+            }),
+          };
+          const response = await fetch("/", options);
+          const json = await response.json();
+          setSearchResults(json);
+        } catch (err) {
+          console.log(err);
+        }
+      }, 200);
+    } else {
+      setSearchMovies(false);
+    }
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [searchInput]);
+
+  return (
+    <Container maxWidth="sm">
+      <Paper
+        onSubmit={searchHandler}
+        component="form"
+        sx={{
+          m: "1rem 1rem",
+          p: "0.25rem 1rem",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <InputBase
+          sx={{ width: "100%" }}
+          inputRef={searchRef}
+          value={searchInput}
+          id="outlined-basic"
+          name="search"
+          label=""
+          placeholder="Search Movies"
+          variant="outlined"
+          onChange={searchInputHandler}
+        />
+        <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+          <span className="material-symbols-outlined">search</span>
+        </IconButton>
+        {searchMovies && <SearchResults results={searchResults} />}
+      </Paper>
+    </Container>
+  );
+};

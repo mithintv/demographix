28f482d12ad8a902386e2b10bb568b6166c3e7a2
Mithin Thomas <mithingthomas@gmail.com>
2023-06-02 13:09:33 -0400

    Add cast scroller under Movie Card

--- api/static/js/components/MovieCard.jsx
+++ api/static/js/components/MovieCard.jsx
@@ -64,7 +64,6 @@ const MovieCard = (props) => {
             </Container>
             <Typography sx={{ my: 1 }}>{movie.overview}</Typography>
 
-            <CastDetails cast={movie.cast} />
           </Container>
         ) : (
           <Container

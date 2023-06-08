777bd4bfbfcc66c17632e84c52c5686d0d4a8496
Mithin Thomas <mithingthomas@gmail.com>
2023-06-08 14:48:29 -0400

    Adjust styling of race chart

--- api/static/js/components/RaceChart.jsx
+++ api/static/js/components/RaceChart.jsx
@@ -1,4 +1,5 @@
 const RaceChart = React.memo((props) => {
+  const theme = useTheme();
   const { data } = props;
 
   console.log("Rendering Race Chart: ", data);
@@ -16,28 +17,40 @@ const RaceChart = React.memo((props) => {
       }}
     >
       <ChartLabel label={"Race"} />
-
-      <BarChart
-        width={525}
-        height={300}
-        data={data}
-        margin={{
-          top: 5,
-          right: 50,
-          left: 10,
-          bottom: 25,
-        }}
-      >
-        {/* <CartesianGrid strokeDasharray="3 3" /> */}
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip
-          viewBox={{ x: 0, y: 0, width: 400, height: 400 }}
-          contentStyle={{ backgroundColor: "black" }}
-        />
-        <Legend iconSize={12} />
-        <Bar dataKey="amount" fill="#8884d8" />
-      </BarChart>
+      <ResponsiveContainer width={550}>
+        <BarChart
+          layout="vertical"
+          width={550}
+          height={300}
+          data={data}
+          margin={{
+            top: 20,
+            right: 25,
+            left: 100,
+            bottom: 10,
+          }}
+        >
+          {/* <CartesianGrid strokeDasharray="3 3" /> */}
+          <XAxis
+            type="number"
+            tickLine={axisLineStyle}
+            axisLine={axisLineStyle}
+            tick={tickStyle}
+          />
+          <YAxis
+            type="category"
+            dataKey="name"
+            tickLine={axisLineStyle}
+            axisLine={axisLineStyle}
+            tick={tickStyle}
+          />
+          <Tooltip
+            viewBox={{ x: 0, y: 0, width: 400, height: 400 }}
+            contentStyle={{ backgroundColor: "black" }}
+          />
+          <Bar dataKey="amount" fill={theme.palette.primary.main} />
+        </BarChart>
+      </ResponsiveContainer>
     </Paper>
   );
 });

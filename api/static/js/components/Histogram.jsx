44e76b10c1578957783a0a09472b0728a9765f1f
Mithin Thomas <mithingthomas@gmail.com>
2023-06-08 12:47:10 -0400

    Change histogram to recharts

--- api/static/js/components/Histogram.jsx
+++ api/static/js/components/Histogram.jsx
@@ -1,75 +1,39 @@
+const CustomTooltip = ({ active, payload, label }) => {
+  if (active && payload && payload.length) {
+    const ageGroup = payload[0].payload.ageGroup;
+    const count = payload[0].value;
+    const names = payload[0].payload.names || [];
+
+    return (
+      <Paper sx={{ p: 2 }}>
+        <Typography variant="overline">{`${ageGroup}: ${count}`}</Typography>
+        <p>{`Names: ${names.join(", ")}`}</p>
+      </Paper>
+    );
+  }
+
+  return null;
+};
+
 const Histogram = React.memo((props) => {
-  const histogramRef = React.useRef(null);
+  const [histogram, setHistogram] = React.useState([]);
   const { data } = props;
 
-  console.log("Rendering Histogram: ", data);
-
   React.useEffect(() => {
-    // set the dimensions and margins of the graph
-    const margin = { top: 10, right: 30, bottom: 30, left: 40 },
-      width = 450 - margin.left - margin.right,
-      height = 300 - margin.top - margin.bottom;
+    const histogramData = d3.range(0, 10).map((i) => ({
+      ageGroup: `${i * 10}-${(i + 1) * 10}`,
+      count: 0,
+      names: [],
+    }));
 
-    // append the svg object to the body of the page
-    const svg = d3
-      .select(histogramRef.current)
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
-
-    // d3.csv(
-    //   "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv"
-    // ).then((data) => {
-
-    // X axis: scale and draw:
-    const x = d3
-      .scaleLinear()
-      .domain([0, 100]) // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-      .range([0, width]);
-
-    svg
-      .append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(d3.axisBottom(x));
-
-    // set the parameters for the histogram
-    const histogram = d3
-      .histogram()
-      .value((d) => d.amount) // I need to give the vector of value
-      .domain(x.domain()) // then the domain of the graphic
-      .thresholds(x.ticks(30)); // then the numbers of bins
-
-    // And apply this function to data to get the bins
-    const bins = histogram(data);
-
-    // Y axis: scale and draw:
-    const y = d3.scaleLinear().range([height, 0]);
-    y.domain([
-      0,
-      d3.max(bins, function (d) {
-        return d.length;
-      }),
-    ]); // d3.hist has to be called before the Y axis obviously
-    svg.append("g").call(d3.axisLeft(y));
-
-    // append the bar rectangles to the svg element
-    svg
-      .selectAll("rect")
-      .data(bins)
-      .join("rect")
-      .attr("x", 1)
-      .attr("transform", (d) => `translate(${x(d.x0)} , ${y(d.length)})`)
-      .attr("width", (d) => x(d.x1) - x(d.x0))
-      .attr("height", (d) => height - y(d.length))
-      .attr("stroke", "white")
-      .style("fill", "#69b3a2");
-    // });
-    return () => {
-      // Remove the SVG element from the DOM
-      d3.select(histogramRef.current).selectAll("svg").remove();
-    };
+    data.forEach((item) => {
+      const ageGroupIndex = Math.floor(item.amount / 10);
+      if (ageGroupIndex >= 0 && ageGroupIndex < histogramData.length) {
+        histogramData[ageGroupIndex].count++;
+        histogramData[ageGroupIndex].names.push(item.name);
+      }
+    });
+    setHistogram(histogramData);
   }, [data]);
 
   return (
@@ -80,22 +44,30 @@ const Histogram = React.memo((props) => {
         m: 2,
         display: "flex",
         flexDirection: "column",
-        justifyContent: "center",
+        justifyContent: "space-between",
+        alignItems: "center",
         backgroundColor: "background.default",
         flexGrow: 1,
       }}
     >
       <ChartLabel label={"Age Distribution"} />
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "330px",
-          width: "100%",
+      <BarChart
+        width={500}
+        height={300}
+        data={histogram}
+        margin={{
+          top: 0,
+          right: 0,
+          bottom: 25,
+          left: -35,
         }}
-        ref={histogramRef}
-      ></Box>
+      >
+        <XAxis dataKey="ageGroup" />
+        <YAxis dataKey="count" />
+        <Tooltip content={<CustomTooltip />} />
+        {/* <Legend iconSize={12} /> */}
+        <Bar dataKey="count" fill="#8884d8" />
+      </BarChart>
     </Paper>
   );
 });

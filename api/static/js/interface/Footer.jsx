6c78b67355e9177d53aefd5fe4fcffc5e7938924
Mithin Thomas <mithingthomas@gmail.com>
2023-06-13 10:34:24 -0400

    Add basic footer and adjust landing page description

---
+++ api/static/js/interface/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        py: 2,
+        backgroundColor: "transparent",
+      }}
+    >
+      <Typography align="center" variant="overline" color="textSecondary">
+        Created by Mithin Thomas
+      </Typography>
+    </Box>
+  );
+};

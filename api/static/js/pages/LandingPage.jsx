d506ad1af3990c1296456092f550d3c1f2e308e0
Mithin Thomas <mithingthomas@gmail.com>
2023-06-13 10:34:24 -0400

    Add basic footer and adjust landing page description

--- api/static/js/pages/LandingPage.jsx
+++ api/static/js/pages/LandingPage.jsx
@@ -12,7 +12,7 @@ const LandingPage = () => {
           display: "flex",
           height: "100vh",
           flexDirection: "column",
-          justifyContent: "center",
+          justifyContent: "space-between",
           background: backgroundGradient,
         }}
       >
@@ -21,6 +21,7 @@ const LandingPage = () => {
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
+            pt: 30,
           }}
         >
           <Link sx={{ textDecoration: "none" }} component={RouterLink} to="/">
@@ -29,15 +30,15 @@ const LandingPage = () => {
             </Typography>
           </Link>
           <Typography
-            sx={{ mt: 2, mb: 4, width: "800px" }}
+            sx={{ mt: 2, mb: 4, width: "950px", lineHeight: 1.25 }}
             color="textSecondary"
-            variant="body"
+            variant="h5"
             align="center"
           >
-            Visualize demographic information of top billed cast in movies
-            nominated for prestigious awards such as the Academy Awards, the
-            Golden Globes, BAFTA, etc. You can also search for demographic
-            breakdowns of cast members in individual productions.
+            Visualize demographics of top billed cast in movies nominated for
+            prestigious awards including the Academy Awards, the Golden Globes,
+            BAFTA, etc. Demographix also provides demographic breakdowns of cast
+            members in individual productions.
           </Typography>
           <Box
             sx={{
@@ -60,6 +61,7 @@ const LandingPage = () => {
             <SearchPage nav={false} />
           </Box>
         </Box>
+        <Footer />
       </Box>
     </Fade>
   );

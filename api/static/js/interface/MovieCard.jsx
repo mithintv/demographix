d701ac0d2ec9e9f74ce5eb512ceaa9503fd67495
Mithin Thomas <mithingthomas@gmail.com>
2023-06-24 12:24:51 -0400

    Add accordian for top billed cast in movie details

--- api/static/js/interface/MovieCard.jsx
+++ api/static/js/interface/MovieCard.jsx
@@ -1,4 +1,9 @@
 const MovieCard = (props) => {
+	const theme = useTheme();
+	const lg = useMediaQuery("(max-width:1200px)");
+	const md = useMediaQuery("(max-width:960px)");
+	const sm = useMediaQuery("(max-width:600px)");
+	const xs = useMediaQuery("(max-width:425px)");
 	const { movie } = props;
 	const [content, setContent] = React.useState(movie);
 	const [show, setShow] = React.useState(false);
@@ -21,51 +26,84 @@ const MovieCard = (props) => {
 				mb: 2,
 				py: 3,
 				px: 3,
-				width: "350px",
+				width: "100%",
 				display: "flex",
 				flexDirection: "column",
 				justifyContent: content && content.id ? "start" : "center",
 				alignItems: content && content.id ? "start" : "center",
-				flex: "1 0 auto",
+				flex: "0 0 auto",
 			}}
 		>
 			{content.id ? (
 				<Fade in={show}>
-					<Container disableGutters>
-						<Typography variant="h5">{content.title}</Typography>
-						<Container
-							disableGutters
-							sx={{
-								p: 0,
-								marginLeft: 0,
-								marginRight: "auto",
-								display: "flex",
-								flexDirection: "row",
-							}}
-						>
-							<Typography sx={{ paddingRight: 1 }} variant="caption">
-								{new Date(content.release_date).getFullYear()}
-							</Typography>
-							<Typography variant="caption">
-								{compileRuntime(content.runtime)}
-							</Typography>
-						</Container>
-						<CardMedia
-							sx={{ my: 2 }}
-							component="img"
-							width={200}
-							image={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${content.poster_path}`}
-							alt={`Movie poster for ${content.title}`}
-						/>
-
-						<Stack direction="row" spacing={1} useFlexGap flexWrap="wrap">
-							{content.genres.map((genre, i) => {
-								return <Chip key={i} label={genre} />;
-							})}
-						</Stack>
-						<Typography variant="subtitle2" sx={{ my: 1 }}>
-							{content.overview}
-						</Typography>
+					<Container
+						disableGutters
+						sx={{
+							display: "flex",
+							flexDirection: "column",
+						}}
+					>
+						<Box sx={{ display: "flex", flexDirection: "row", mb: 2 }}>
+							<Box
+								sx={{
+									display: "flex",
+									flexDirection: "column",
+								}}
+							>
+								<CardMedia
+									component="img"
+									width={"100%"}
+									image={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${content.poster_path}`}
+									alt={`Movie poster for ${content.title}`}
+								/>
+							</Box>
+							<Box
+								sx={{
+									display: "flex",
+									flexDirection: "column",
+								}}
+							>
+								<Typography
+									variant="h5"
+									sx={{
+										mx: 2,
+									}}
+								>
+									{content.title}
+								</Typography>
+								<Container
+									disableGutters
+									sx={{
+										px: 2,
+										marginLeft: 0,
+										marginRight: "auto",
+										display: "flex",
+										flexDirection: "row",
+									}}
+								>
+									<Typography sx={{ paddingRight: 1 }} variant="caption">
+										{new Date(content.release_date).getFullYear()}
+									</Typography>
+									<Typography variant="caption">
+										{compileRuntime(content.runtime)}
+									</Typography>
+								</Container>
+								<Stack
+									sx={{ mt: 2, ml: 2 }}
+									direction="row"
+									spacing={1}
+									useFlexGap
+									flexWrap="wrap"
+								>
+									{content.genres.map((genre, i) => {
+										return <Chip key={i} label={genre} />;
+									})}
+								</Stack>
+								<Typography variant="subtitle2" sx={{ m: 2 }}>
+									{content.overview}
+								</Typography>
+							</Box>
+						</Box>
 					</Container>
 				</Fade>
 			) : (
